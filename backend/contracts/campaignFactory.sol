00e0f3884b951b3235b963dfef7f37bf64f2ec9c
Ankesh2004 <gankesh808@gmail.com>
2024-08-08 18:19:19 +0530

    fix[campaignFactory.sol] : only registered NGO can create campaign

--- backend/contracts/campaignFactory.sol
+++ backend/contracts/campaignFactory.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import "./Campaign.sol";
+import "./ngoRegistry.sol";
 
 contract CampaignFactory  {
     event CampaignCreated(uint goal, address benefitedNGO, address campaignAddress);
     address [] public campaigns;
     mapping (address => bool) public activeCampaign;
 
-    function createCampaign(uint goal, address ngo) public {
-        require(msg.sender == ngo, "Only NGO can create campaign");
-        Campaign newCampaign = new Campaign(goal, ngo);
+    function createCampaign(uint goal) public {
+        require(NGORegistry(msg.sender).isRegisteredNGO(msg.sender), "NGO not registered");
+        Campaign newCampaign = new Campaign(goal, msg.sender);
         campaigns.push(address(newCampaign));
         activeCampaign[address(newCampaign)] = true;
-        emit CampaignCreated(goal,ngo,address(newCampaign);
+        emit CampaignCreated(goal,msg.sender,address(newCampaign));
     }
 
     function getActiveCampaigns() public view returns (address[] memory) {
